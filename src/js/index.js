3a0f9903b1f5eed7c5bf1a3884ed76f5adda5922
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2023-05-14 01:50:47 -0500

    Nice UI Changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,31 @@
-var errors = ""
-
 const button = document.getElementById("magic-button")
 
+const actionBar = document.getElementById("action-bar")
+
+const editorWindow = document.getElementById("editor-window")
 const rawEditor = document.getElementById("editing")
 const stylizedCode = document.getElementById("code")
 const lineNumbers = document.getElementById("line-numbers")
+
+const terminalWindow = document.getElementById("terminal-window")
+const terminalCode = document.getElementById("terminal-code")
+
+// const 
+
+var errors = ""
+
+const starterTabs = []
+starterTabs.push({
+    "title": "Untitled",
+    "type": "code",
+    "text": ""
+})
+
+starterTabs.push({
+    "title": "Terminal",
+    "type": "terminal",
+    "text": ""
+})
 
 const log = console.log
 
@@ -13,243 +34,75 @@ function onScroll()
     stylizedCode.scrollLeft = rawEditor.scrollLeft
     stylizedCode.scrollTop = rawEditor.scrollTop
     lineNumbers.scrollTop = rawEditor.scrollTop
-
-    // log(rawEditor.scrollTop + " " + stylizedCode.scrollTop)
 }
 
-function colorCode(text, color)
+function codeUpdate()
 {
-    return `<span style="color: ${color};">${text}</span>`
-}
-
-function getTextWidth(text, font) {
-    // re-use canvas object for better performance
-    const canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
-    const context = canvas.getContext("2d");
-    context.font = font;
-    const metrics = context.measureText(text);
-    return metrics.width;
-  }
-
-function getCssStyle(element, prop) {
-    return window.getComputedStyle(element, null).getPropertyValue(prop);
-}
-
-function getCanvasFont(el = document.body) {
-  const fontWeight = getCssStyle(el, 'font-weight') || 'normal';
-  const fontSize = getCssStyle(el, 'font-size') || '16px';
-  const fontFamily = getCssStyle(el, 'font-family') || 'Times New Roman';
-  
-  return `${fontWeight} ${fontSize} ${fontFamily}`;
-}
-
-const validIdentifiers = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-const stringStarters = "\"`'"
-
-const keywordcolor = "red"
-const termcolor = "blue"
-const parenthesesColor = 'yellow'
-const bracesColor = "#ae3fbf" 
-const operatorcolor = "orange"
-const stringColor = "#86eb93"
-const commentColor = "grey"
-const propertyColor = "#86ceeb"
-const lineColor = "red"
-
-const keywords = {
-    // Keywords
-    "for": keywordcolor,
-    "foreach": keywordcolor,
-    "let": keywordcolor,
-    "const": keywordcolor,
-    "var": keywordcolor,
-    "function": keywordcolor,
-    "if": keywordcolor,
-    "else": keywordcolor,
-    "return": keywordcolor,
-
-    // Terms
-    "document": termcolor,
-    "console": termcolor,
-    
-    // Symbols
-    "(": parenthesesColor,
-    ")": parenthesesColor,
-    "{": bracesColor,
-    "}": bracesColor,
-
-    // Operators
-    "&": operatorcolor,
-    "|": operatorcolor,
-    "=": operatorcolor,
-    "+": operatorcolor,
-    "-": operatorcolor,
-    "*": operatorcolor,
-    "/": operatorcolor,
-
-    ":": operatorcolor,
-    ".": operatorcolor,
-    ";": operatorcolor,
-}
-
-function isValidChar(text)
-{
-    return validIdentifiers.indexOf(text) != -1
-}
-
-function isValidString(text)
-{
-    for (let i = 0; i < text.length; i++)
-    {
-        if (!isValidChar(text.substring(i, i+1)))
-            return false;
-    }
-    return true
-}
-
-function removeThans(value)
-{
-    return value.replaceAll("<", "&lt").replaceAll(">", "&gt")
-}
-
-const commentEndings = {"//": "\n", "/*": "*/"}
-
-function styleCode(code)
-{
-    let parts = []
-    let i = 0;
-    while (true)
-    {
-        let firstChar = code.substring(i, i+1)
-
-        
-        // Comments
-        if (code.length > i+1 && commentEndings[code.substring(i, i+2)] != undefined)
-        {
-            let ending = commentEndings[code.substring(i, i+2)]
-            let end = code.indexOf(ending, i)
-
-            if (end == -1)
-            {
-                // Sad ending
-                let str = code.substring(i, code.length)
-                parts.push(str)
-                i+=str.length;
-            } else {
-                let str = code.substring(i, end+ending.length)
-                parts.push(str)
-                i+=str.length-1;
-            }
-        }
-        // Strings
-        else if (stringStarters.indexOf(firstChar)!=-1)
-        {
-            let str = firstChar
-            let slashCancel
-            while(code.length > i+1 && (code.substring(i+1, i+2) != firstChar || slashCancel))
-            {
-                if (slashCancel)
-                    slashCancel = false
-                else 
-                    slashCancel = code.substring(i+1, i+2)=="\\"
-                str += code.substring(i+1, i+2)
-                i++;
-            }
-
-            if (code.length <= i+1)
-            {
-                parts.push(str)
-                break
-            } else {
-                parts.push(str+firstChar)
-                i += 1;
-            }
-        }
-        else if (isValidChar(firstChar))
-        {
-            let indetifierString = firstChar
-            while(code.length > i+1 && isValidChar(code.substring(i+1, i+2)))
-            {
-                indetifierString += code.substring(i+1, i+2)
-                i++;
-            }
-            parts.push(indetifierString)
-        } else {
-            parts.push(firstChar)
-        }
-
-        i++;
-        if (code.length <= i)
-            break;
-
-    }
-
-    // log(code)
-    // log(parts)
-
-    let newcode = ""
-    for (let i = 0; i < parts.length; i++)
-    {
-        const part = parts[i]
-        const prepart = i > 0 && parts[i-1]
-        if (part.length > 1 && commentEndings[part.substring(0, 2)])
-            newcode += colorCode(removeThans(part), commentColor)
-        else if (stringStarters.indexOf(part.substring(0, 1)) != -1)
-            // Strings
-            newcode += colorCode(removeThans(part), stringColor)
-        else if (keywords[part])
-            // Keywords and stuff
-            newcode += colorCode(removeThans(part), keywords[part])
-        else if (isValidString(part) && prepart==".")
-            // Stuff after the dot
-            newcode += colorCode(removeThans(part), propertyColor)
-        else 
-            // Normal
-            newcode += removeThans(part)
-    }
-
-    // Line Numbers
-    const lineCount = (newcode.match(/\n/g) || []).length+1;
-    const spaceCount = Math.ceil(Math.log10(lineCount))
-    let lined = ""
-    log(spaceCount)
-    for (let i = 0; i < lineCount; i++)
-    {
-        // Getting number of spaces needed
-        let lineTag = ``
-        for (let j = `${i+1}`.length; j < spaceCount; j++)
-            lineTag += " "
-
-        lineTag += `${i+1}  `
-
-        lined += lineTag+"\n"
-    }
-
-    let spaceBuffer = ""
-    for (let i = 0; i < spaceCount+2; i++)
-    {
-        spaceBuffer += " "
-    }
-
-    const textWidth = getTextWidth(spaceBuffer, getCanvasFont(document.getElementById("code")))
-    // lineNumbers.style.width = `calc(${textWidth}px)`
-    lineNumbers.innerHTML = colorCode(lined, lineColor)
-
-    rawEditor.style.width    = `calc(100% - ${textWidth}px)`
-    stylizedCode.style.width = `calc(100% - ${textWidth}px)`
-    rawEditor.style.left     = `${textWidth}px`
-    stylizedCode.style.left  = `${textWidth}px`
-
-    newcode += "\n\n\n"
-
-    return newcode
-}
-
-rawEditor.oninput = () => {
     onScroll()
-    stylizedCode.innerHTML = styleCode(rawEditor.value) 
+    const styles = styleCode(rawEditor.value) 
+
+    lineNumbers.innerHTML = styles.lined
+
+    rawEditor.style.width    = styles.editorWidth
+    stylizedCode.style.width = styles.editorWidth
+    rawEditor.style.left     = styles.editorLeft
+    stylizedCode.style.left  = styles.editorLeft
+
+    stylizedCode.innerHTML = styles.code
+
+    currentTab.text = rawEditor.value
 }
 
+let currentTab;
+function setCurrentTab(tab)
+{
+    currentTab.tab.classList.remove("selected")
+    currentTab = tab
+    tab.tab.classList.add("selected")
+
+    editorWindow.style.visibility = "hidden"
+    terminalWindow.style.visibility = "hidden"
+
+    switch (tab.type)
+    {
+        case "code":
+            rawEditor.value = tab.text
+            codeUpdate()
+            editorWindow.style.visibility = "visible"
+            break;
+        case "terminal":
+            terminalWindow.style.visibility = "visible"
+            break;
+    }
+
+    rawEditor.value = tab.text
+}
+
+function createTab(tabData)
+{
+    const tab = document.createElement('span')
+    tab.classList.add("edit-font")
+    tab.classList.add("action-tab")
+    actionBar.appendChild(tab)
+    tab.innerHTML = tabData.title
+    const data = {
+        "tab": tab,
+        ...tabData
+    }
+
+    tab.onclick = () => setCurrentTab(data)
+
+    tabs.push(data)
+}
+
+const tabs = []
+
+starterTabs.map(createTab)
+
+currentTab = tabs[0]
+setCurrentTab(tabs[0])
+
+rawEditor.oninput = codeUpdate
 rawEditor.onscroll = onScroll
 
 
